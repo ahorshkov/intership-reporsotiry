ab4f2ff4906c3908c6b80cd1dc1c62df65bdb74e
admin <admin@admins-mbp.mynetworksettings.com>
2024-06-05 17:35:47 +0300

    task AQA-33 browserstack commit

--- features/environment.py
+++ features/environment.py
@@ -17,16 +17,16 @@ def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    # driver_path = './chromedriver.exe'
-    # context.driver = webdriver.Chrome(executable_path=driver_path)
+     #driver_path = './chromedriver.exe'
+    #context.driver = webdriver.Chrome(executable_path=driver_path)
     # #
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
-
-    driver_path = GeckoDriverManager().install()
+    driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    context.driver = webdriver.Chrome(service=service)
+
+    #driver_path = GeckoDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Firefox(service=service)
 
     ### BROWSERS WITH DRIVERS: provide path to the driver file ###
     # service = Service(executable_path='/Users/svetlanalevinsohn/careerist/18-python-selenium-automation/geckodriver')
@@ -46,21 +46,32 @@ def browser_init(context, scenario_name):
     #)
 
     ### BROWSERSTACK ###
+
+    ## BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    # bs_user = 'lenakuletsky_SKaVUT'
-    # bs_key = 'W9FLyyLLHFBYCqawWphz'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    #
-    # options = Options()
-    # bstack_options = {
-    #     'osVersion': '10.0',
-    #     # 'os': 'OS X',
-    #     'deviceName': 'Samsung Galaxy Note 20 Ultra',
-    #     'browserName': 'chrome',
-    #     'sessionName': scenario_name
-    # }
-    # options.set_capability('bstack:options', bstack_options)
-    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    bs_user = 'horshkovandrii_eKmYwc'
+    bs_key = '3xyM47URfcye9sGyzgju'
+    #bs_user = '********'
+    #bs_key = '*********'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        'os': 'Windows',
+        'osVersion': '10',
+        'browserName': 'Chrome',
+        'os': 'OS X',
+        'osVersion': 'Sonoma',
+        #        'osVersion': 'Windows',
+        'browserName': 'Safari',
+        #         'browserName': 'Chrome',
+        'browserVersion': '17.3',
+        #         'browserVersion': 'latest_version',
+        'sessionName': 'Test AQA_33'
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
